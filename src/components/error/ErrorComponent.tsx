18cf6c7e3f7e9dad7cc12f5dcb8874e45fd6daf4
letniki <nikitaletuta12@gmai.com>
2025-02-02 20:14:53 +0200

    add styles

--- src/components/error/ErrorComponent.tsx
+++ src/components/error/ErrorComponent.tsx
@@ -1,10 +1,7 @@
-import {Link} from "react-router-dom";
-
 export const ErrorComponent = () => {
     return (
         <div>
             <h2>Error, check url </h2>
-            <Link className='Link' to={'/auth/login'}><h2>Or click here to go to login page</h2></Link>
         </div>
     );
 };
