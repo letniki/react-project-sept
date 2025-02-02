68e180e8b7bfee64a122ec0720573b004f6ffb67
letniki <nikitaletuta12@gmai.com>
2025-02-02 20:10:29 +0200

    add styles

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -6,9 +6,10 @@ import {AuthRecipesPage} from "../pages/authRecipesPage/AuthRecipesPage.tsx";
 import {UserCardComponent} from "../components/userCard/UserCardComponent.tsx";
 import {RecipeCard} from "../components/recipeCard/RecipeCard.tsx";
 import {RecipesByTagComponent} from "../components/recipesByTag/RecipesByTagComponent.tsx";
+import {ErrorComponent} from "../components/error/ErrorComponent.tsx";
 
 export const routes = createBrowserRouter([
-    {path:'', element:<MainLayout/>, children:[
+    {path:'', element:<MainLayout/>, errorElement:<ErrorComponent/>, children:[
             {path:'/auth/users', element:<AuthUsersPage/>},
             {path:'/login', element:<LoginPage/>},
             {path:'/auth/users/:id', element:<UserCardComponent/>},
